ad0ab263531de4f556cf8107f0db597cefc4f4f3
rafaelChelo <rafael@chelofinance.io>
2024-01-26 15:04:25 -0400

    feat: scripts initial

--- scripts/index.ts
+++ scripts/index.ts
@@ -10,8 +10,9 @@ dotenv.config();
 
 const main = async () => {
   const args = yargs(process.argv.slice(2)).options({
-    network: {type: "string", demandOption: false, alias: "n", desc: "chain to interact with"},
-    action: {type: "string", demandOption: false, alias: "a", desc: "action"},
+    network: {type: "string", demandOption: true, alias: "n", desc: "chain to interact with"},
+    action: {type: "string", demandOption: true, alias: "a", desc: "action"},
+
     contract: {type: "string", demandOption: false, alias: "sc", desc: "contract"},
     codeId: {type: "number", demandOption: false, desc: "code id of contract"},
     message: {type: "string", demandOption: false, desc: "message to send to contract"},
@@ -20,10 +21,10 @@ const main = async () => {
 
   if (args.message) args.message = getMessage(args.message as any);
 
-  const network = config.networks[args.chain];
+  const network = config.networks[args.network];
   const action = actions[args.action];
 
-  if (!network) throw new Error(`network ${args.chain} not in config file`);
+  if (!network) throw new Error(`network ${args.network} not in config file`);
   if (!action) throw new Error(`action ${args.action} not implemented`);
 
   const signer = await loadSignerFromMnemonic(network);
