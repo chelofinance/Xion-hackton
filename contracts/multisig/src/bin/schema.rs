c90caf0ad60293335286c2f95ed139bbb88494d3
rafaelChelo <rafael@chelofinance.io>
2024-01-26 11:57:20 -0400

    feat: basic cw3 impl

--- contracts/multisig/src/bin/schema.rs
+++ contracts/multisig/src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use cw_template_minimal::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw3_fixed_multisig::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
