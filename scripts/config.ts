92c487dd50db891b654d926b2b8d4480570ba5d0
rafaelChelo <rafael@chelofinance.io>
2024-01-29 12:47:01 -0400

    feat: added relayer config. Working relayer

--- scripts/config.ts
+++ scripts/config.ts
@@ -5,7 +5,7 @@ const config: Config = {
     "xion-testnet": {
       chainId: "xion-testnet-1",
       node: "https://rpc.xion-testnet-1.burnt.com:443",
-      gasPrice: "0.1uxion",
+      gasPrice: "0.001uxion",
       gasAdjustment: 1.3,
       prefix: "xion",
       denom: "uxion",
