2f25fa05452ab31a93e677c62eb6b56d57b220f3
rafaelChelo <rafael@chelofinance.io>
2024-02-12 11:38:40 -0400

    feat: custom relay of channel creation (ica account)

--- scripts/query_ica.sh
+++ scripts/query_ica.sh
@@ -1,1 +1,1 @@
-yarn start --action query --network xion-testnet --message '{"get_contract_state":{}}' --contract xion1sc37w8thh9szjl74y8cje59z0amh6wra7lpmfs30lel9r2wrpa9spfcy90 
+yarn start --action query --network xion-testnet --message '{"get_contract_state":{}}' --contract xion1yylkaz0u4gef9n77m3dmarq6wmjwgzlhrrveu45cv9neh582m36q0aq33m
